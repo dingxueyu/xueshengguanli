6f6f3b8c78fc0d7f1a2ea277eee8f8fc58876f8b
xueyu Ding <2941778794@qq.com>
2022-04-26 20:23:48 +0800

    master

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+    system("pause");
+    return 0;
+}
+
+//你好，我好大家好，大家好
+
+  
+ //这是一个跑通了的一个很好的demo，最起码exe文件出现之后，没有出现闪退，是一个非常大的进步
